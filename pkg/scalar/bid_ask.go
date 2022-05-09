2903c1a576fa146b292f00310d671cc1d694c9e9
Preston Vasquez <prestonvs10@gmail.com>
2022-05-08 21:55:55 -0600

    add comment to bid ask

--- pkg/scalar/bid_ask.go
+++ pkg/scalar/bid_ask.go
@@ -4,4 +4,5 @@ package scalar
 // given time. The term "ask" refers to the lowest price at which a seller will sell the stock. The bid price will
 // almost always be lower than the ask or “offer,” price.
 
+// BidAsk is a slice of bytes that represents the bids or asks for a given product.
 type BidAsk []byte
